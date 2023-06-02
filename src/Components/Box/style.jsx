537b46ec6d943f96e8525a68a8d4e3b7b1385bbf
bourdain <romain.bourdain@unistra.fr>
2023-06-02 16:45:08 +0200

    initialisation

--- src/Components/Box/style.jsx
+++ src/Components/Box/style.jsx
@@ -4,8 +4,6 @@ export const BoxContainer = styled.div`
   display: flex;
   text-align: center;
   justify-content: center;
-  /* min-width: 100vw; */
-  /* min-height: 100vh; */
   padding: 10px;
 `;
 
